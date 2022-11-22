19d57b1194cddeed227d1862f6701c918dc6286f
1valerich1@mail.ru <1valerich1@mail.ru>
2022-11-22 11:43:47 +0400

    added task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c && a != b && a != c) Console.Write(a);
+else if (b > a && b > c && b != a && b != c) Console.Write(b);
+else if (c > a && c > b && c != a && c != b) Console.Write(c);
