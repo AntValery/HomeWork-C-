baba8d17696ae0095acb7cb77d480b708fbe3ee8
1valerich1@mail.ru <1valerich1@mail.ru>
2022-11-22 11:34:46 +0400

    added task2

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) Console.Write("max = " + a);
+else if (b > a) Console.Write("max = " + b);
